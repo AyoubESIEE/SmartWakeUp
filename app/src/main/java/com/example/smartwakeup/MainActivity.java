68dee6d00d3152861868fa0be7ad09ed8af33887
Ayoub <ayoub.meski@edu.esiee.fr>
2021-10-29 16:59:06 +0200

    MAJ 29/10

--- app/src/main/java/com/example/smartwakeup/MainActivity.java
+++ app/src/main/java/com/example/smartwakeup/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.smartwakeup;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView myText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent presActivityIntent = new Intent(MainActivity.this, PresActivity.class);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                startActivity(presActivityIntent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, 3000);
+
+
+
+
     }
 }
