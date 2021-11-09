b0b2bd6e3fd6bc02cb8c46cabb2240538c9cd218
Ayoub <ayoub.meski@edu.esiee.fr>
2021-11-09 16:59:32 +0100

    MAJ 9/11

--- app/src/main/java/com/example/smartwakeup/PresActivity.java
+++ app/src/main/java/com/example/smartwakeup/PresActivity.java
@@ -1,13 +1,34 @@
 package com.example.smartwakeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PresActivity extends AppCompatActivity {
+
+    private Button mNextButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pres);
+        mNextButton = findViewById(R.id.boutton_suivant);
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //setContentView(R.layout.activity_pres);
+                Intent impActivityIntent = new Intent(PresActivity.this, ImportActivity.class);
+                startActivity(impActivityIntent);
+
+            }
+        });
+
+
+
+
     }
 }
