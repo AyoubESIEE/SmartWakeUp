848f6898d2063bbe185f6e5f9fc827d6d83bf089
Ayoub <ayoub.meski@edu.esiee.fr>
2021-12-16 23:23:31 +0100

    MAJ 16/12

--- app/src/main/java/com/example/smartwakeup/ImportActivity.java
+++ app/src/main/java/com/example/smartwakeup/ImportActivity.java
@@ -9,13 +9,14 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ImportActivity extends AppCompatActivity {
-    private Button Popol,pipil;
+    private Button Popol,pipil, TimePick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import);
         Popol = findViewById(R.id.boutton_import_google);
         pipil = findViewById(R.id.boutton_import_url);
+        TimePick = findViewById(R.id.boutton_timepicker);
         Popol.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +32,16 @@ public class ImportActivity extends AppCompatActivity {
                 //setContentView(R.layout.activity_pres);
                 Intent AlarmListActivity = new Intent(ImportActivity.this, AlarmListActivity.class);
                 startActivity(AlarmListActivity);
+
+            }
+        });
+
+        TimePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //setContentView(R.layout.activity_pres);
+                Intent AlarmConfig = new Intent(ImportActivity.this, AlarmConfig.class);
+                startActivity(AlarmConfig);
 
             }
         });
